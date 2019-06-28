540947ccb77c80c71138e1cba0e9dc138dc27c44
kirill-larionov <36547280+kirill-larionov@users.noreply.github.com>
2019-06-29 00:02:39 +0300

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -16,7 +16,7 @@ mv newmain.m test_koef.m
 octave --no-gui test_koef.m > out
 variant=$(awk -F: '{if ($1 == "task_number") {print $2}}' out)
 check_linear=$(awk -F: '{if ($1 == "linear_polynom_koef") {print $2}}' out)
-correct_linear=$(awk -F: '{if ($1 == $variant) {print $2}}' ./tests/var_linear)
+correct_linear=$(awk -F: '{if ($1 == '$variant') {print $2}}' ./tests/var_linear)
 echo "ТЕСТ №2: Проверка коэффициентов линейного полинома"
 if [ "$correct_linear" != "$check_linear" ]
 then
